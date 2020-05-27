3690c8f6441db7a1714383f02b5fad506c108c08
fritzgt <tuanis@live.com>
2020-05-27 08:35:28 +0300

    create Model

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Model/Contacts.swift
@@ -0,0 +1,20 @@
+//
+//  Contacts.swift
+//  Contacts-Hybrid
+//
+//  Created by FGT MAC on 5/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+class Contacts {
+    var name: String
+    var relationship: String?
+    
+    init(name: String, relationship: String?) {
+        self.name = name
+        self.relationship = relationship
+    }
+    
+}
