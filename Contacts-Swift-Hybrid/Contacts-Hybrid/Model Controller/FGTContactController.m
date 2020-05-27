d50f4faef5b69c20c11a25b41b18e8eb6020910c
fritzgt <tuanis@live.com>
2020-05-27 10:40:10 +0300

    create implementation for contact

--- Contacts-Swift-Hybrid/Contacts-Hybrid/Model Controller/FGTContactController.m
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Model Controller/FGTContactController.m
@@ -10,4 +10,26 @@
 
 @implementation FGTContactController
 
+
+//Because getter is overwriten and readonly, we need to tell complile to make
+//a "backing" instance variable named _contacts
+//Getter
+@synthesize contacts = _contacts;
+//Lazy init
+- (NSArray<Contact *> *)contacts {
+    if(!_contacts){//In nil then initialize it
+        _contacts = [[NSArray alloc] init];
+    }
+    return _contacts;
+};
+
+//Init
+- (instancetype)init {
+    self = [super init];
+    if(self){
+        
+    }
+    return self;
+}
+
 @end
